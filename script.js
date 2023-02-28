99b709e4f3b103c1399ca347c98924f8a362d7cf
kanakkhandelwal25~ <kkhandelwal1590@gmail.com>
2023-02-28 22:17:58 +0530

    final commit

--- script.js
+++ script.js
@@ -16,21 +16,23 @@ const changeTurn = () => {
 const checkWin = () => {
     let boxTexts = document.getElementsByClassName('boxtext');
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0,1,2,5,5,0],
+        [3,4,5,5,15,0],
+        [6,7,8,5,25,0],
+        [0,3,6,-5,5,90],
+        [1,4,7,5,15,90],
+        [2,5,8,5,25,90],
+        [0,4,8,5,15,45],
+        [2,4,6,5,15,135]
     ]
 
     wins.forEach((e) => {
         if((boxTexts[e[0]].innerText === boxTexts[e[1]].innerText) && (boxTexts[e[1]].innerText === boxTexts[e[2]].innerText) && (boxTexts[e[0]].innerText !== '')){
-            document.querySelector('.info').innerText = boxTexts[e[0]].innerText + "Won"
+            document.querySelector('.info').innerText = boxTexts[e[0]].innerText + " Won"
             gameOver = true;
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '150px';
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '200px';
+            document.querySelector('.line').style.width = '20vw';
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`
         }
 })
 }
@@ -51,6 +53,21 @@ Array.from(boxes).forEach((element) => {
         }
     })
 })
+
+reset.addEventListener('click',() => {
+    let boxTexts = document.querySelectorAll('.boxtext');
+
+    Array.from(boxTexts).forEach((element) => {
+        element.innerText = "";
+    })
+    turn = "X";
+    gameOver = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '0px';
+    document.querySelector('.line').style.width = '0px';
+})
+
+
 
 
 
